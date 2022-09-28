a071886de527df95c8bea1636d3f22291d7a0bd3
Germán Rico <germanrico@gmail.com>
2022-09-28 10:56:52 -0300

    primer commit con maqueta del sitio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { PicturesComponent } from './components/pictures/pictures.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    AboutmeComponent,
+    ProductsComponent,
+    CoursesComponent,
+    PicturesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
